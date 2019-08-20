f66bd83854a63219b6b427051f39844b0335a7f0
Neha Patel <neha.r.p@gmail.com>
2019-08-20 02:55:27 -0700

    displays correct text and red when locked and closed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,12 +2,42 @@
 import React from "react";
 import Display from "./Display";
 import renderer from "react-test-renderer";
+import "@testing-library/jest-dom/extend-expect";
+import { render, cleanup } from "@testing-library/react";
+
+afterEach(cleanup);
 
 describe("<Display />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Display />);
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it("should display 'Closed' if the 'closed' prop is true", () => {
+     const { getByText } = render(<Display closed={true} />);
+     expect(getByText(/closed/i)).toBeTruthy()
+  })
+
+  it("should display 'Open' if the 'closed' prop is false", () => {
+    const { getByText } = render(<Display closed={false} />);
+    expect(getByText(/open/i)).toBeTruthy()
+ })
+
+ it("should display 'Locked' if the 'locked' prop is true", () => {
+    const { getByText } = render(<Display locked={true} />);
+    expect(getByText(/locked/i)).toBeTruthy()
+ })
+
+ it("should display 'Unlocked' if the 'locked' prop is false", () => {
+    const { getByText } = render(<Display locked={false} />);
+    expect(getByText(/unlocked/i)).toBeTruthy()
+ })
+
+  it("should use 'red-led' class when 'locked' or 'closed'", () => {
+      const { getByText } = render(<Display locked={true} closed={true} />)
+      expect(getByText(/locked/i)).toHaveClass('red-led');
+      expect(getByText(/closed/i)).toHaveClass('red-led')
+  })
 });
 
 // displays if gate is open/closed and if it is locked/unlocked
