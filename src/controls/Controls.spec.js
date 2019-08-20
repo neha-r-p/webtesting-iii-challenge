4cdb59ac43897c2f71b5b5dfe23edaad82f55334
Neha Patel <neha.r.p@gmail.com>
2019-08-20 01:31:19 -0700

    controls...buttons display unlock gate and open gate if state is currently locked and closed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,27 +1,32 @@
+import React from "react";
+import Controls from "./Controls";
+import renderer from "react-test-renderer";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, cleanup } from "@testing-library/react";
 
-import React from 'react';
-import Controls from './Controls';
-import renderer from 'react-test-renderer';
-import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render } from '@testing-library/react';
-// import '@testing-library/react/cleanup-after-each';
+afterEach(cleanup);
 
 describe("<Controls />", () => {
-    it("matches snapshot", () => {
-      const tree = renderer.create(<Controls />); 
-      expect(tree.toJSON()).toMatchSnapshot();
-    });
-
-    it("should provide buttons to toggle closed and locked states", () => {
-        render(<Controls />)
-        const toggleButtons = document.querySelectorAll('.toggle-btn')
-        expect(toggleButtons.length).toEqual(2)
-    })
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 
-// describe("lock and unlock gate")
+  it("should provide buttons to toggle closed and locked states", () => {
+    render(<Controls />);
+    const toggleButtons = document.querySelectorAll(".toggle-btn");
+    expect(toggleButtons.length).toEqual(2);
+  });
 
-//toggle closed and locked states
+  it("should display buttons for unlock gate and open gate if state is locked and closed", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+    expect(getByText(/unlock gate/i));
+    expect(getByText(/open gate/i));
+  });
+
+  
+});
+
 //button's text changes to reflect state
 //closed toggle button is disabled if gate is locked
 //the locked toggle button is disabled if gate is open
